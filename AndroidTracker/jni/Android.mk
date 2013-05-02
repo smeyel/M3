85212bc2ab9249d423824966cff69f1c1f488b63
scorpeeon <scorpeeon@gmail.com>
2013-05-03 01:20:48 +0200

    AndroidTracker updated (added include paths for C++ in project settings for external sources)

--- AndroidTracker/jni/Android.mk
+++ AndroidTracker/jni/Android.mk
@@ -1,7 +1,9 @@
+#this points to the root of the project where the external native sources are
 LOCAL_PATH := $(call my-dir)/../..
 
 include $(CLEAR_VARS)
 
+#this should point to OpenCV Android SDK
 include ../../../android-opencv/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE := native_module
@@ -17,4 +19,3 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/Framework/libMiscTimeAndConfig/include \
 LOCAL_LDLIBS +=  -llog -ldl
 
 include $(BUILD_SHARED_LIBRARY)
-
