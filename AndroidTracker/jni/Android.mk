ebbe300a0fd0cfffaa98e10cbf3e172586a118b5
scorpeeon <scorpeeon@gmail.com>
2013-05-03 13:16:27 +0200

    Framework subrepo updated

--- AndroidTracker/jni/Android.mk
+++ AndroidTracker/jni/Android.mk
@@ -10,12 +10,10 @@ LOCAL_MODULE := native_module
 FILE_LIST := $(wildcard $(LOCAL_PATH)/Framework/libMiscTimeAndConfig/src/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/ImageProcessing/libTwoColorCircleMarker/src/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/AndroidTracker/jni/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/AndroidTracker/jni/libMiscTimeAndConfig/src/*.cpp)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/Framework/libMiscTimeAndConfig/include \
 					$(LOCAL_PATH)/ImageProcessing/libTwoColorCircleMarker/include \
-					$(LOCAL_PATH)/AndroidTracker/jni \
-					$(LOCAL_PATH)/AndroidTracker/jni/libMiscTimeAndConfig/include
+					$(LOCAL_PATH)/AndroidTracker/jni
 LOCAL_LDLIBS +=  -llog -ldl
 
 include $(BUILD_SHARED_LIBRARY)
