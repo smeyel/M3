42f99c15a0e227c7ae2cec6eef8e0ccf30423423
Kristof <kristof@aut.bme.hu>
2014-02-13 20:06:50 +0100

    VS2013 upgrade, JPEG receiving test

--- M3Host/src/main.cpp
+++ M3Host/src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+//#include "opencv2/highgui.h"
+#include <opencv2/opencv.hpp>
+
 #include "PhoneProxy.h"
 #include "stdlib.h"
+#include "StdoutLogger.h"
 //#include "myconfigmanager.h"
+#include "JpegMessage.h"
 
 using namespace std;
 
@@ -10,28 +15,38 @@ int main()
 	
 	bool running = true;
 	int frameIdx = 0;
+	LogConfigTime::StdoutLogger logger;
 
 	PhoneProxy proxy;
 	cout << "Connecting..." << endl;
 	//proxy.Connect("192.168.0.8", 6000);
 	proxy.Connect("127.0.0.1", 6000);
 
-	while(running)
+	cv::Mat image(480, 640, CV_8UC3);
+	while (running)
 	{
 		
 		
 		for(int i = 0; ; i++)
 		{
-				
-			cout << "Position No " << i << "..." << endl;
-			cout << "Requesing position..." << endl;
+			cout << "Requesing image..." << endl;
 			
-			proxy.RequestPosition();
+			proxy.RequestPhoto(0);
+
+			cout << "Receiving photo..." << endl;
 			
-			cout << "Receiving position..." << endl;
-			
-			proxy.Receive("");
-			Sleep(500);
+			JsonMessage *msg = proxy.ReceiveNew();
+			JpegMessage *imgMsg = (JpegMessage*)msg;
+			imgMsg->Decode(&image);
+			cv::rectangle(image, cv::Rect(20, 20, 100, 100), cv::Scalar(255, 0, 0));
+			cv::imshow("PICTURE", image);
+
+			//proxy.Receive("result.txt");
+			char ch = cv::waitKey(25);
+			if (ch == 27)
+			{
+				break;
+			}
 		}
 
 		cout << "Disconnecting..." << endl;
