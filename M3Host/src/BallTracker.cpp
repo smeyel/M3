075f4e77e4d9ea4d14a5b4260c71e5d5bdb5da7a
Fuge <g.furnstahl@gmail.com>
2014-03-05 20:55:57 +0100

    Find and draw circles.

--- M3Host/src/BallTracker.cpp
+++ M3Host/src/BallTracker.cpp
@@ -10,8 +10,24 @@ void BallTracker::init(const char *configfilename){
 }
 
 void BallTracker::processFrame(Mat& img){
-	
-
+	/*HSV:0-180, 0-97, 201-256 */
+	Mat imgHSV,imgThresh;
+	cvtColor(img, imgHSV, CV_BGR2HSV); //Change the color format from BGR to HSV
+	inRange(imgHSV, Scalar(0, 0, 201), Scalar(180, 97, 256), imgThresh);
+	//kontúr keresés
+	vector<vector<Point>> kontur;
+	findContours(imgThresh, kontur, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+	for (int i = 0; i < kontur.size(); i++)
+	{
+		if (kontur[i].size()>40)
+		{
+			float radius;
+			Point2f center;
+			minEnclosingCircle(kontur[i], center, radius);
+			circle(img, (Point2i)(center), (int)radius, Scalar(255, 0, 0));
+		}
+	}
+	imshow("pic", img);
 }
 
 
