dfeea9c7568d496bf067db9544c3449f7923caaf
johnfabian28 <johnfabian28@gmail.com>
2014-04-02 10:48:51 +0200

    Adding getLastPoint() method and comments.

--- M3Host/include/ObjectTracker.h
+++ M3Host/include/ObjectTracker.h
@@ -7,16 +7,33 @@
 #include <opencv2/core/mat.hpp>
 
 #include "TimeMeasurement.h"
+#include "camera.h"
 using namespace cv;
 using namespace LogConfigTime;
+
+/*
+Tracker Base interface.
+*/
 class ObjectTracker
 {
 public:
+	Camera *camera;
 	TimeMeasurement *timeMeasurement=NULL;
 	ObjectTracker();
 
+	/*
+	Initializing the tracker from the given file.
+	*/
 	virtual void init(const char *configfilename)=0;
-	
+
+	/*
+	Returns the last found point.
+	*/
+	virtual Point2f getLastPoint()=0;
+
+	/*
+	Processing a frame with the tracker.
+	*/
 	virtual void processFrame(Mat &src)=0;
 
 	~ObjectTracker();
